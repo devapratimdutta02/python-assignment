1e2c58dbbb11dad5f67c81692318cfc2585d0a68
devapratimdutta02 <devapratimdutta785@gmail.com>
2024-11-06 00:58:40 +0530

    Delete python_task1_movie rental system directory

--- python_task1_movie rental system/earnings.py
+++
@@ -1,8 +0,0 @@
-total_earnings = 0
-
-def add_earning(amount):
-    global total_earnings
-    total_earnings +=amount
-    
-def calculate_earnings():
-    print(f"Total rental earnings: {total_earnings}")
