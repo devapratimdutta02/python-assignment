84f2e50c5acb44ecff33742b801d195598135ac1
devapratimdutta02 <devapratimdutta785@gmail.com>
2024-11-11 00:58:45 +0530

    Update classScoreFiles.py

--- file handling/classScoreFiles.py
+++ file handling/classScoreFiles.py
@@ -1,14 +1,17 @@
-file1 = open("C:/Users/ccsad/OneDrive/Desktop/Documents/D/class_score.txt", "r")
+file1 = open("class_score.txt", "r")
 read_content = file1.readlines()
 file1.close()
 
 new_content = []
 
 for line in read_content:
-    username, score = line.split()
-    new_score = int(score) + 5
-    new_content.append(f"{username} {new_score}\n")
+    if line.strip():
+        parts=line.split()
+        if len(parts)==2:
+            username, score = parts
+            new_score = int(score) + 5
+            new_content.append(f"{username} {new_score}\n")
 
-file2 = open("C:/Users/ccsad/OneDrive/Desktop/Documents/D/class_score_2.txt", "w")
+file2 = open("class_score_2.txt", "w")
 file2.writelines(new_content)
 file2.close()
